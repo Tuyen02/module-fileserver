6b8b8837515061c1d1e0a50e05846b5cb6705f02
Tuyen02 <hoangtuyen2442002@gmail.com>
2025-01-16 23:02:48 +0700

    Cập nhật admin: cấp quyền cho nhóm người

--- modules/fileserver/admin/main.php
+++ modules/fileserver/admin/main.php
@@ -1,42 +1,43 @@
 <?php
 
-/**
- * NukeViet Content Management System
- * @version 4.x
- * @author VINADES.,JSC <contact@vinades.vn>
- * @copyright (C) 2009-2021 VINADES.,JSC. All rights reserved
- * @license GNU/GPL version 2 or any later version
- * @see https://github.com/nukeviet The NukeViet CMS GitHub project
- */
-
 if (!defined('NV_IS_FILE_ADMIN')) {
     exit('Stop!!!');
 }
 $page_title = 'FILE SERVER';
 
+$sql = "SELECT d.group_id, title FROM nv4_users_groups AS g LEFT JOIN nv4_users_groups_detail d 
+        ON g.group_id = d.group_id AND d.lang = '" . NV_LANG_DATA . "' 
+        WHERE g.idsite = " . $global_config['idsite'] . " OR (g.idsite = 0 AND g.group_id > 3 AND g.siteus = 1) 
+        ORDER BY g.idsite, g.weight ASC";
+$result = $db->query($sql);
 $post = [];
 $mess = '';
+$post['group_ids'] = $nv_Request->get_array('group_ids', 'post', []);
 
 if ($nv_Request->isset_request('submit', 'post')) {
-    $post['config_value'] = $nv_Request->get_title('config_value', 'post', '');
-
-    if ($post['config_value'] == '') {
-        $mess = 'Chua nhap';
-    }
-    $config_name = 'group_admin_fileserver';
-    $lang = 'vi';
-    $sql = "INSERT INTO nv4_config(lang, module,config_name,config_value) 
-                VALUES (:lang, :module, :config_name,:config_value)
+    if (empty($post['group_ids'])) {
+        $mess = 'Chưa chọn nhóm nào.';
+    } else {
+        $group_ids_str = implode(',', $post['group_ids']);
+        $config_name = 'group_admin_fileserver';
+        $lang = 'vi';
+        $sql = "INSERT INTO nv4_config(lang, module, config_name, config_value) 
+                VALUES (:lang, :module, :config_name, :config_value)
                 ON DUPLICATE KEY UPDATE 
                         config_value = :config_value_update";
-    $stmt = $db->prepare($sql);
-    $stmt->bindParam(':lang', $lang, PDO::PARAM_STR);
-    $stmt->bindParam(':module', $module_name, PDO::PARAM_STR);
-    $stmt->bindParam(':config_name', $config_name, PDO::PARAM_STR);
-    $stmt->bindParam(':config_value', $post['config_value'], PDO::PARAM_STR);
-    $stmt->bindParam(':config_value_update', $post['config_value'], PDO::PARAM_STR);
-    $exe = $stmt->execute();
-    $mess = 'Update oke';
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':lang', $lang, PDO::PARAM_STR);
+        $stmt->bindParam(':module', $module_name, PDO::PARAM_STR);
+        $stmt->bindParam(':config_name', $config_name, PDO::PARAM_STR);
+        $stmt->bindParam(':config_value', $group_ids_str, PDO::PARAM_STR);
+        $stmt->bindParam(':config_value_update', $group_ids_str, PDO::PARAM_STR);
+        if ($exe = $stmt->execute()) {
+            $mess = 'Cập nhật thành công.';
+        }else{
+            $mess = 'Cập nhật thất bại.';
+        }
+        
+    }
 }
 
 $xtpl = new XTemplate('main.tpl', NV_ROOTDIR . '/themes/' . $global_config['module_theme'] . '/modules/' . $module_file);
@@ -49,6 +50,11 @@ $xtpl->assign('NV_OP_VARIABLE', NV_OP_VARIABLE);
 $xtpl->assign('MODULE_NAME', $module_name);
 $xtpl->assign('OP', $op);
 $xtpl->assign('POST', $post);
+
+foreach ($result as $row) {
+    $xtpl->assign('ROW', $row);
+    $xtpl->parse('main.loop');
+}
 
 if ($mess != '') {
     $xtpl->assign('MESSAGE', $mess);
--- themes/admin_default/modules/fileserver/main.tpl
+++ themes/admin_default/modules/fileserver/main.tpl
@@ -8,10 +8,21 @@
     <form
         action="{NV_BASE_ADMINURL}index.php?{NV_LANG_VARIABLE}={NV_LANG_DATA}&amp;{NV_NAME_VARIABLE}={MODULE_NAME}&amp;{NV_OP_VARIABLE}={OP}"
         method="post" class="confirm-reload">
+        <h3>Chọn nhóm người được phép truy cập</h3>
         <div class="form-group">
             <div>
-                <label><strong>Nhập mã nhóm:</strong></label>
-                <input class="form-control" type="text" value="{POST.config_value}" name="config_value">
+                <div>
+                    <!-- BEGIN: loop -->
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text">
+                                <input type="checkbox" name="group_ids[]" value="{ROW.group_id}" id="group_{ROW.group_id}">
+                                <label for="group_{ROW.group_id}">{ROW.title}</label>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- END: loop -->
+                </div>
             </div>
             <br>
             <button type="submit" class="btn btn-primary" value="1" name="submit">Xác nhận</button>
